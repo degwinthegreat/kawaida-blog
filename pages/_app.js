c37e393ebb8accc699abdb4d6c73eebdbc262bf0
degwinthegreat <the.colledge.dropout@gmail.com>
2021-01-04 22:39:36 +0900

    material-ui導入

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../components/theme';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
